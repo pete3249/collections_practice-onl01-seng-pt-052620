eb5ab57635aca00d68b1289ff7d6b802a611a699
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-03 19:53:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+require 'pry'
+
+def sort_array_asc(array)
+  array.sort!
+end 
+
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end 
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length}
+end 
+
+def swap_elements(array)
+  second_value = array[1]
+  array[1] = array[2]
+  array[2] = second_value
+  array
+end 
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |string|
+    seperated_string = string.split("")
+    seperated_string[2] = "$"
+    new_array << seperated_string.join
+  end 
+  new_array
+end 
+
+def find_a(array)
+  array.select {|item| item.start_with? "a"}
+end 
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end 
+
+def add_s(array)
+  array.each_with_index.collect do |element, index| 
+    if index != 1
+      element + "s"
+    else
+      element
+    end
+  end
+end 
 
